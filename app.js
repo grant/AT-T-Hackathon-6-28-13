192c0ca1ca060e24236aa1e89ef5e4bf15ac1cf3
Grant Timmerman <granttimmerman@gmail.com>
2013-06-29 17:42:17 -0700

    almost done

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function($) {
 
 // Feed
 
-var json = '{"Gatzert Elementary School": {"Address": "1301 East Yesler Wy Seattle WA 98122", "lat" : "47.60184560", "lng" : "-122.31528850", "Opportunity": "Math tutor for 3rd/4th grade class.", "Description": "Need volunteer to tutor 3rd and 4th graders on fractions.", "Number": "1", "Date": "Tuesdays", "Time": "2:00-4:00 PM", "Skills": ["Math","Any"], "Points": "300"}, "Gatzert Elementary School": {"Address": "1301 East Yesler Wy Seattle WA 98122", "lat" : 47.60184560, "lng" : -122.31528850, "Opportunity": "Plant and weed flowerbeds.", "Description": "Come help us clean up and replant the front flower beds.  Bring garden tools and gloves. Appropriate for students, under 13 with parent supervision", "Number": "20", "Date": "Saturday April 6", "Time": "noon-4:00 PM", "Skills": ["Any"], "Points": "600"}, "Lowell Elementary School": {"Address": "1058 East Mercer St Seattle WA 98102", "lat" : 47.6247020, "lng" : -122.3183070, "Opportunity": "General school tutoring", "Description": "Need more helpers for our after school study lab.  Students need help in a variety of subjects.", "Number": "5", "Date": "Monday-Thursday", "Time": "3:00-4:30 PM", "Skills": ["Math","Science","Computer","Languages","Any"], "Points": "600"}, "Bacon Hill Elementary School": {"Address": "2025 14 Ave South Seattle WA 98144", "lat" : 47.5850830, "lng" : -122.3153340, "Opportunity": "Help in music class", "Description": "Do you know how to read music or play a musical instrument?  Please come and help students in our music program.", "Number": "2", "Date": "Tuesdays", "Time": "10:00-11:00 AM", "Skills": ["Music"], "Points": "100"}, "Leschi Elementary School": {"Address": "135 32 Ave Seattle WA 98122", "lat" : 47.6019580, "lng" : -122.2919510, "Opportunity": "Drivers for field trip to museum", "Description": "We still need drivers for our trip to the Pacific Science Center!  District driving clearance required. Let us know how many children can fit in your car, with seat belts.", "Number": "4", "Date": "April 8", "Time": "10:00 AM - 4:00 PM", "Skills": ["Any"], "Points": "500"}, "Thurgood Marshall Elementary School": {"Address": "2401 South Irving St Seattle WA", "lat" : 47.5909610, "lng" : -122.2992640, "Opportunity": "Lunch room and recess monitor", "Description": "Looking for one more person to monitor students at lunch and recess.  No experience necessary.", "Number": "1", "Date": "Monday-Friday", "Time": "11:00 AM - 1:00 PM", "Skills": ["Any"], "Points": "200"}, "Thurgood Marshall Elementary School": {"Address": "2401 South Irving St Seattle WA", "lat" : 47.5909610, "lng" : -122.2992640, "Opportunity": "Science lab helper", "Description": "Help me prevent my chemistry class from lighting the class on fire with their bunsen burners.  Thanks much.  Mr. Nye.", "Number": "1", "Date": "Tuesday,Thursday", "Time": "9:30-10:30", "Skills": ["Science","Any"], "Points": "200"}, "Thurgood Marshall Elementary School": {"Address": "2401 South Irving St Seattle WA", "lat" : 47.5909610, "lng" : -122.2992640, "Opportunity": "School drop-off safety monitor", "Description": "Direct traffic and keep our students safe during drop off in the morning.  Volunteers can sign up for one or more days.  Whatever time they can donate.", "Number": "3", "Date": "Monday-Friday", "Time": "7:00-7:30 AM", "Skills": ["Any"], "Points": "100"}}';
+var json = '{"Gatzert Elementary School":{"Address":"1301 East Yesler Wy Seattle WA 98122", "lat":"47.60184560", "lng":"-122.31528850", "Opportunity":"Math tutor for 3rd/4th grade class.", "Description":"Need volunteer to tutor 3rd and 4th graders on fractions.", "Number":"1", "Date":"Tuesdays", "Time":"2:00-4:00 PM", "Skills":["Math", "Any"], "Points":"300"}, "Gatzert Elementary School":{"Address":"1301 East Yesler Wy Seattle WA 98122", "lat":47.60184560, "lng":-122.31528850, "Opportunity":"Plant and weed flowerbeds.", "Description":"Come help us clean up and replant the front flower beds.  Bring garden tools and gloves. Appropriate for students, under 13 with parent supervision", "Number":"20", "Date":"Saturday April 6", "Time":"noon-4:00 PM", "Skills":["Any"], "Points":"600"}, "Lowell Elementary School":{"Address":"1058 East Mercer St Seattle WA 98102", "lat":47.6247020, "lng":-122.3183070, "Opportunity":"General school tutoring", "Description":"Need more helpers for our after school study lab.  Students need help in a variety of subjects.", "Number":"5", "Date":"Monday-Thursday", "Time":"3:00-4:30 PM", "Skills":["Math", "Science", "Computer", "Languages", "Any"], "Points":"600"}, "Bacon Hill Elementary School":{"Address":"2025 14 Ave South Seattle WA 98144", "lat":47.5850830, "lng":-122.3153340, "Opportunity":"Help in music class", "Description":"Do you know how to read music or play a musical instrument?  Please come and help students in our music program.", "Number":"2", "Date":"Tuesdays", "Time":"10:00-11:00 AM", "Skills":["Music"], "Points":"100"}, "Leschi Elementary School":{"Address":"135 32 Ave Seattle WA 98122", "lat":47.6019580, "lng":-122.2919510, "Opportunity":"Drivers for field trip to museum", "Description":"We still need drivers for our trip to the Pacific Science Center!  District driving clearance required. Let us know how many children can fit in your car, with seat belts.", "Number":"4", "Date":"April 8", "Time":"10:00 AM - 4:00 PM", "Skills":["Any"], "Points":"500"}, "Thurgood Marshall Elementary School":{"Address":"2401 South Irving St Seattle WA", "lat":47.5909610, "lng":-122.2992640, "Opportunity":"Lunch room and recess monitor", "Description":"Looking for one more person to monitor students at lunch and recess.  No experience necessary.", "Number":"1", "Date":"Monday-Friday", "Time":"11:00 AM - 1:00 PM", "Skills":["Any"], "Points":"200"}, "Thurgood Marshall Elementary School":{"Address":"2401 South Irving St Seattle WA", "lat":47.5909610, "lng":-122.2992640, "Opportunity":"Science lab helper", "Description":"Help me prevent my chemistry class from lighting the class on fire with their bunsen burners.  Thanks much.  Mr. Nye.", "Number":"1", "Date":"Tuesday,Thursday", "Time":"9:30-10:30", "Skills":["Science", "Any"], "Points":"200"}, "Thurgood Marshall Elementary School":{"Address":"2401 South Irving St Seattle WA", "lat":47.5909610, "lng":-122.2992640, "Opportunity":"Science lab helper", "Description":"Help me prevent my chemistry class from lighting the class on fire with their bunsen burners.  Thanks much.  Mr. Nye.", "Number":"1", "Date":"Tuesday,Thursday", "Time":"9:30-10:30", "Skills":["Science", "Any"], "Points":"200"}, "Washington Middle School":{"Address":"2101 South Jackson St Seattle WA", "lat":47.5978260, "lng":-122.3037060, "Opportunity":"Math Tutor for Advanced Algebra", "Description":"Can you spare a few hours a week to help students struggling with Advanced Algebra topics?  Thanks.", "Number":"1", "Date":"Open", "Time":"Open", "Skills":["Math"], "Points":"100"}, "Mclure Middle School":{"Address":"1915 1 Av West Seattle WA", "lat":47.63571870, "lng":-122.35830320, "Opportunity":"Career Day Presenters", "Description":"Do you have an interesting job?  Would love to have you share at our career fair.", "Number":"12", "Date":"Wednesday April 12", "Time":"8:00-10:00 PM", "Skills":["Any"], "Points":"200"}, "Mclure Middle School":{"Address":"1915 1 Av West Seattle WA", "lat":47.63571870, "lng":-122.35830320, "Opportunity":"Girl\'s Basketball Coach", "Description":"Need assistant coach for girls\' basketball team.", "Number":"1", "Date":"Monday-Thursday", "Time":"3:00-4:30 PM", "Skills":["Any"], "Points":"600"}, "Hamilton International Middle School":{"Address":"4400 Interlake Avenue North Seattle WA", "lat":47.66013820, "lng":-122.33983810, "Opportunity":"Front office helper", "Description":"Answer phones and check in school visitors.", "Number":"1", "Date":"Monday-Friday", "Time":"7:30-8:30 AM", "Skills":["Any"], "Points":"500"} }';
 var schools = $.parseJSON(json);
 var schoolKeys = Object.keys(schools);
 
@@ -45,6 +45,11 @@ $(function() {
     // Results circle
     $('.resultsCircle').find('.number').html(schoolKeys.length);
   }
+
+  // Location List
+  $('.locationList li').click(function(e) {
+    $(e.currentTarget).addClass('selected');
+  });
 });
 
 // 
@@ -102,19 +107,16 @@ function setupGeo(lat,lng){
     });
 
     marker.setMap(map);
-    
-
-    google.maps.event.addDomListener(window, 'load', initialize);
 }
 
 function filter() {
-    document.getElementById("googleMap").innerHTML = "";
+  document.getElementById("googleMap").innerHTML = "";
     // clear map
    var mapOptions = {
-            zoom: 3,
-            center: new google.maps.LatLng(11,11),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-          };
+      zoom: 12,
+      center: new google.maps.LatLng(47.600743,-122.3314),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
     map = new google.maps.Map(document.getElementById('googleMap'),mapOptions);
     // make new map
@@ -134,10 +136,27 @@ function filter() {
         marker.setMap(map);
     }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
-}
+    // List results
+    var $ul = $('.locationList');
+    $ul.html('');
 
+  for (var i = 4; i < schoolKeys.length-1; ++i) {
+    var schoolName = schoolKeys[i];//string
+    var schoolData = schools[schoolName];//object
 
-function initialize() {
-  infowindow = new google.maps.InfoWindow();
+    var li = '<li>' +
+        '<div class="locationFirstRow">' +
+          '<div class="numberCircle">' + (i - 3) + '</div>' +
+          '<h1 class="locationTitle">' + schoolData.Opportunity + '</h1>' +
+          '<h2 class="locationLocation">' + schoolName + ": " + schoolData.Address + '</h2>' +
+        '</div>' +
+        '<div class="locationSecondRow">' +
+          '<p>'+schoolData.Description+'</p>' +
+        '</div>' +
+      '</li>';
+    $ul.append(li);
+
+    // Results circle
+    $('.resultsCircle').find('.number').html(3);
+  }
 }
